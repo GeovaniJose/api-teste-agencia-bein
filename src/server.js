2ed3b98c2d91c7bf20505ebea1601424d55d99f4
Geovani José <geovani-lol@hotmail.com>
2019-08-23 23:52:18 -0300

    Implement mongoose

--- src/server.js
+++ src/server.js
@@ -1,7 +1,18 @@
+require('dotenv/config')
 const express = require('express')
+const mongoose = require('mongoose')
 
+// Iniciando o app
 const app = express()
-
 app.use(express.json())
+
+// Iniciando o DB
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-hsbag.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false
+  })
 
 app.listen(3333, () => console.log('Server listening...'))
