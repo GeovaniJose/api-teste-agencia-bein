6f15865e8db8330f00f37c904c8c3dcae1616e04
Geovani José <geovani-lol@hotmail.com>
2019-08-24 04:50:52 -0300

    Add cors to server

--- src/server.js
+++ src/server.js
@@ -1,16 +1,19 @@
 require('dotenv/config')
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 const routes = require('./routes')
 
 // Iniciando o app
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 // Iniciando o DB
 mongoose.connect(
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-hsbag.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-hsbag.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority` ||
+  `mongodb://${process.env.DB_HOST}:27017/${process.env.DB_NAME}`,
   {
     useCreateIndex: true,
     useNewUrlParser: true,
@@ -20,4 +23,4 @@ mongoose.connect(
 // Rotas
 app.use('/api', routes)
 
-app.listen(3333)
+app.listen(process.env.PORT || 3333)
