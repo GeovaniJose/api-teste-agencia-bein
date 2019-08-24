0becd65cf682e78d2e9e015be4936efd91e014d7
Geovani José <geovani-lol@hotmail.com>
2019-08-24 02:52:23 -0300

    Implement route /authenticate

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const UserController = require('./controllers/UserController')
+const AuthController = require('./controllers/AuthController')
 
 const routes = express.Router()
 
 routes.post('/register', UserController.store)
+routes.post('/authenticate', AuthController.store)
 
 module.exports = routes
