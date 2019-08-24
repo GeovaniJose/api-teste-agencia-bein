134f59ed2edf752b1afc21e01b3c806b2409ccda
Geovani José <geovani-lol@hotmail.com>
2019-08-24 00:26:02 -0300

    Implement controller UserController

---
+++ src/controllers/UserController.js
@@ -0,0 +1,23 @@
+const User = require('../models/User')
+
+module.exports = {
+  async store (req, res) {
+    const { email } = req.body
+
+    try {
+      const userExists = await User.findOne({ email })
+
+      if (userExists) {
+        return res.status(400).json({ error: 'User already exists' })
+      }
+
+      const user = await User.create(req.body)
+
+      user.password_hash = undefined
+
+      return res.json({ user })
+    } catch (err) {
+      return res.status(400).json({ error: 'Registration failed' })
+    }
+  }
+}
